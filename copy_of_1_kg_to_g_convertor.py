66931f68efbe339f70e15c8abd9d664d98559d60
Niloofarss <102596038+Niloofarss@users.noreply.github.com>
2022-03-29 23:16:34 +0430

    Update copy_of_1_kg_to_g_convertor.py

--- copy_of_1_kg_to_g_convertor.py
+++ copy_of_1_kg_to_g_convertor.py
@@ -7,6 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1ZTLk0FnAj8LblIXK7kJkGs1MCocOoy7t
 """
 
-kg = input("Please inter kg:")
+kg = input("Please enter kg:")
 g = int(kg) * 1000
 print ("g =", g )
